29a99f2c2b37592f7cd9cb76bc1360036731300d
dengjiawen <dengjiawen8955@gmail.com>
2023-02-24 20:26:29 +0800

    feat: 更新 Install 脚本

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
-go build -o gitbook-summary && cp gitbook-summary ~/go/bin/
-rm -rf gitbook-summary
-echo "gitbook-summary installed"
+# Install gitbook-summary
+if ! command gitbook-summary -v &> /dev/null
+then
+    # If have golang installed, use go install
+    if command go version &> /dev/null
+    then
+        go install github.com/dengjiawen8955/gitbook-summary@latest
+    else
+        # Download binary from github.com/dengjiawen8955/gitbook-summary/bin
+        set -e
+        wget https://github.com/dengjiawen8955/gitbook-summary/raw/master/bin/gitbook-summary
+        sudo mv gitbook-summary /usr/local/bin
+        sudo chmod +x /usr/local/bin/gitbook-summary
+    fi
+    printf "gitbook-summary installed successfully\n"
+fi
+gitbook-summary -v
